c2865c5a3d47ee810b3e54983f98dee3e9f2abbc
Subhash Chandran <subwiz@gmail.com>
2025-08-14 06:46:05 +0530

    go 1.25 upgrade; logging fns removed; WriteResp() added for JSON response

--- response.go
+++ response.go
@@ -2,42 +2,18 @@ package apigo
 
 import (
 	"encoding/json"
-	"io"
-	"log"
 	"net/http"
 )
-
-func LogResp(r *http.Response) {
-	LogRespErr(r, nil)
-}
-
-func LogRespErr(r *http.Response, err error) {
-	if err == nil && r == nil {
-		return
-	}
-	if err != nil && r != nil {
-		log.Println("[api-resp-status-err]", r.Status, "|", err)
-	} else if err != nil && r == nil {
-		log.Println("[api-resp-err]", err)
-	} else if err == nil && r != nil {
-		log.Println("[api-resp-status]", r.Status)
-	}
-
-	// Body print:
-	if r == nil {
-		return
-	}
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(string(b))
-}
 
 func BindResp(r *http.Response, v any) error {
 	if err := json.NewDecoder(r.Body).Decode(v); err != nil {
 		return err
 	}
 	return nil
+}
+
+func WriteResp(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
 }
